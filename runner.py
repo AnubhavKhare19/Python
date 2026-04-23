5988bc92d79d6d15b4ca514d7f6f4cd4a141cbc7
AnubhavKhare19 <khareanubhav05@gmail.com>
2026-04-23 10:25:42 -0700

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,11 @@
+num1 = 20
+num2 = 50
+print("the difference of two given no. is ",num2 - num1)
+
+print("the multiplication of two given no. is ",num2 * num1)
+
+print("the division of two given no. is ",num2 // num1)
+
+print("the division of two given no. is ",num2 / num1)
+
+print("the division of two given no. is ",num2 % num1)
